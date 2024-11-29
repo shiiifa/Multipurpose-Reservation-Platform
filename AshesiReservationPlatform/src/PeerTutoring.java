84fda439dee8cc39389df2122c4f3dfff6498915
BradleyLNO <bradleylamptey7@gmail.com>
2024-11-29 21:23:19 +0000

    Update PeerTutoring.java

--- AshesiReservationPlatform/src/PeerTutoring.java
+++ AshesiReservationPlatform/src/PeerTutoring.java
@@ -2,8 +2,8 @@ public class PeerTutoring extends MeetUp {
     private String courseName;
     private String studentName;
 
-    public PeerTutoring(String courseName, String studentName){
-        super(sessionName, guest);
+    public PeerTutoring(String sessionName, String guest, Time time, Date date, String url, String courseName, String studentName){
+        super(sessionName, guest, time, date, url);
         this.courseName=courseName;
         this.studentName=studentName;
 
