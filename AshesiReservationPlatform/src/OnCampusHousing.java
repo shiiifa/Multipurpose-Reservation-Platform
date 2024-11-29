01def66d57a004cbdaa853eb3949c094baa28b52
shiiiifa <shifa.gabbey@ashesi.edu.gh>
2024-11-29 17:52:21 +0000

    Changes made

--- AshesiReservationPlatform/src/OnCampusHousing.java
+++ AshesiReservationPlatform/src/OnCampusHousing.java
@@ -5,7 +5,10 @@ public class OnCampusHousing extends RemoteReservation {
     private String roomType;
     private List<String> currentOccupants;
 
+    public OnCampusHousing (String roomName, String location, int currentOccupancy, String roomType, List<String> currentOccupants){
+        super(roomName, location);
 
+    }
     public int getCurrentOccupancy() {
         return currentOccupancy;
     }
